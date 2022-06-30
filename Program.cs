2cc6c7cbddcd9bfec02f378772396e5b59f7d637
Yavuz Eroğlu <106495712+yavuzeroglu@users.noreply.github.com>
2022-06-30 17:25:45 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace hackerrank_time_conversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        string s = Console.ReadLine();
+
+        string result = Result.timeConversion(s);
+
+        Console.WriteLine(result);
+
+        
+        }
+    }
+
+    class Result
+    {
+        public static string timeConversion(string s)
+        {
+            var dt =DateTime.ParseExact(
+                s, "hh:mm:sstt",
+            CultureInfo.InvariantCulture);
+            
+            return $"{dt:HH:mm:ss}";
+        }
+    }
+}
